5c33683e08b88c4eb22744bcb44ba4670b4714f8
Denis Volnenko <denis@volnenko.ru>
2025-12-12 05:15:51 +0400

    Обновление проекта.

--- structurizer/schema.sh
+++ structurizer/schema.sh
@@ -1,1 +1,2 @@
-curl https://kroki.io/structurizr/svg --data-raw "$(cat schema.dsl)" > schema.svg
+#curl https://kroki.io/structurizr/svg --data-raw "$(cat schema.dsl)" > schema.svg
+curl https://kroki.volnenko.ru/structurizr/svg --data-raw "$(cat schema.dsl)" > schema.svg
